fdb9a74fc74faa7a67389c6861e55d6056b3e091
Rachel Coulter <rachelecoulter13@gmail.com>
2023-05-19 13:28:23 -0400

    implemented delete function on scenes

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -13,6 +13,17 @@ class ScenesController < ApplicationController
       @scene = Scene.find(params[:id])
     end
 
+    def destroy
+      puts "params = #{params}"
+      @scene = Scene.find(params[:id])
+      @scene.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to quest_url(Quest.find(params[:quest_id])), notice: "Scene was successfully deleted." }
+        format.json { head :no_content }
+      end
+    end
+
     private
       def scene_params
         params.require(:scene).permit(:title, :body)
